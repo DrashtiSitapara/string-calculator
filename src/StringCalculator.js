6f01d6f72090bb314b80f34a82aa2f20b983cec0
Drashti Sitapara <drashtisitapara20@gmail.com>
2025-07-04 08:25:02 +0530

    Handled Newline Delimiter

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -8,6 +8,7 @@ class StringCalculator {
     this.callCount++;
     if (numbers === "") return 0;
 
+    numbers=numbers.replace(/\n/g,",");
     const numArray = numbers.split(",").map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
   }
--- tests/StringCalculator.test.js
+++ tests/StringCalculator.test.js
@@ -11,13 +11,21 @@ describe("StringCalculator", () => {
   test("should return 0 for empty string", () => {
     expect(calc.add("")).toBe(0);
   });
-  
+
   test("should return number if only one number is provided", () => {
   expect(calc.add("1")).toBe(1);
 });
 
 test("should return sum if two numbers are provided", () => {
   expect(calc.add("1,2")).toBe(3);
+});
+
+test("should return sum for multiple comma-separated numbers", () => {
+  expect(calc.add("1,2,3,4")).toBe(10);
+});
+
+test("should handle new line between numbers", () => {
+  expect(calc.add("1\n2,3")).toBe(6);
 });
 
 });
